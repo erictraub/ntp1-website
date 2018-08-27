5a450d6525fd79cc9e587ff1f84a5c0947218983
Eric Traub <est2114@columbia.edu>
2018-08-27 14:19:16 -0400

    update address page

--- browser/js/address/address.state.js
+++ browser/js/address/address.state.js
@@ -6,11 +6,23 @@ app.config(function ($stateProvider) {
         templateUrl: 'js/address/address.template.html',
         resolve: {
         	AddressInfo: function($stateParams, NeblioAPIFactory, TransactionFactory) {
+                let utxoTxsAndData = {};
                 return NeblioAPIFactory.fetchNTP1AddressInfo($stateParams.address)
                 .then(data => {
-                    const utxoTxsAndData = TransactionFactory.formatUtxosAsTransactionsAndBalances(data.utxos);
+                    utxoTxsAndData = TransactionFactory.formatUtxosAsTransactionsAndBalances(data.utxos);
+                    const tokenPromises = [];
+                    utxoTxsAndData.allTokenIds.forEach(id => {
+                        tokenPromises.push(NeblioAPIFactory.fetchAllTokenMetaData(id));
+                    });
+                    return Promise.all(tokenPromises);
+                }).then(tokensData => {
+                    const tokensDataObject = {};
+                    tokensData.forEach(token => {
+                        tokensDataObject[token.tokenId] = token;
+                    });
+                    utxoTxsAndData.tokensDataObject = tokensDataObject;
                     return utxoTxsAndData;
-                })
+                });
             }
         }
     });
