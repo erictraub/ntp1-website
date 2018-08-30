f725db625b0a38cc3561362ffbe8f393496eba65
Eric Traub <est2114@columbia.edu>
2018-08-30 19:50:45 -0400

    update search bar functionality and ui

--- browser/js/common/directives/search-bar/search-bar.js
+++ browser/js/common/directives/search-bar/search-bar.js
@@ -9,11 +9,27 @@ app.directive('searchbar', function ($rootScope, AuthService, $state) {
         	scope.searchBy = null;
 
         	scope.search = function() {
+                if (!searchFormValid()) return;
         		if (scope.searchBy === 'address') $state.go('address', { 'address': scope.searchText });
         		else if (scope.searchBy === 'block') $state.go('block', { 'blockIdentifier': scope.searchText });
         		else if (scope.searchBy === 'token') $state.go('token', { 'tokenIdentifier': scope.searchText });
         		else if (scope.searchBy === 'tx') $state.go('transaction', { 'txId': scope.searchText });
         	};
+
+            function searchFormValid() {
+                $('form .ng-invalid').addClass('redRing');
+                $('form .ng-valid').removeClass('redRing');
+                console.log(scope.searchForm)
+                return scope.searchForm.$valid;
+            };
+
+            function addRedRing(indentifier) {
+                $(indentifier).addClass('redRing');
+            };
+
+            function removeRedRing(indentifier) {
+                $(indentifier).removeClass('redRing');
+            }
         }
     };
 
