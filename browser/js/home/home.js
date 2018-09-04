068d73be7ba34a2c82d1686ba802de577f775828
Eric Traub <est2114@columbia.edu>
2018-09-04 19:10:31 -0400

    add latest block to home page

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -6,12 +6,20 @@ app.config(function ($stateProvider) {
         resolve: {
             PopularTokens: function(NeblioAPIFactory) {
                 return NeblioAPIFactory.fetchPopularTokens();
+            },
+            LatestBlock: function(NeblioAPIFactory) {
+                return NeblioAPIFactory.fetchLatestBlock()
+                .then(latestBlockData => {
+                    return NeblioAPIFactory.fetchBlockByHash(latestBlockData.lastblockhash);
+                });
             }
         }
     });
 });
 
 
-app.controller('HomeController', function ($scope, PopularTokens) {
+app.controller('HomeController', function ($scope, PopularTokens, LatestBlock) {
 	$scope.PopularTokens = PopularTokens;
+    $scope.LatestBlock = LatestBlock;
+    console.log('LatestBlock', LatestBlock);
 });
